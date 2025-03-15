364f9c48c454fba70a4691126fd5b869ee603726
Justin Garza <jgarza9788@gmail.com>
2025-03-15 17:34:56 -0400

    20250315

--- reports_to_html.bat
+++ reports_to_html.bat
@@ -1,3 +1,5 @@
 ::jupyter nbconvert --execute --to html main.ipynb    --output docs/index.html        --HTMLExporter.theme=dark
 jupyter nbconvert --execute --to html main.ipynb    --output docs/index.html        --HTMLExporter.theme=dark 
 
+
+::jupyter nbconvert --execute --to html main.ipynb --template misc/custom.tpl --output docs/index.html --HTMLExporter.theme=dark 
